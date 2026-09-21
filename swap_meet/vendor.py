c991a80de9fac7f6f2c81be6b2aa2a066cdcc996
Yevheniia Sholokh <yevheniiasholokh@Yevheniias-MacBook-Air.local>
2026-09-21 14:39:33 -0700

    modify vendor.py for wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -15,5 +15,10 @@ class Vendor:
             return item
         else:
             return None
-        
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None    
     
