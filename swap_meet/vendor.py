2ed8612b1d5e0108d0411363fb2a7c2b3385d62d
Yevheniia Sholokh <yevheniiasholokh@Yevheniias-MacBook-Air.local>
2026-09-21 15:35:00 -0700

    modify vendor.py for wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -31,7 +31,24 @@ class Vendor:
             other_vendor.remove(their_item)
             other_vendor.add(my_item)        
             return True
+
+    def swap_first_item(self, other_vendor):
+        if len(self.inventory) == 0:
+            return False    
+        if len(other_vendor.inventory) == 0:
+            return False
         
+        my_first_item = self.inventory[0]
+        their_first_item = other_vendor.inventory[0]
+
+        self.remove(my_first_item)
+        other_vendor.remove(their_first_item)
+
+        self.add(their_first_item)
+        other_vendor.add(my_first_item)
+
+        return True
+
         
 
     
