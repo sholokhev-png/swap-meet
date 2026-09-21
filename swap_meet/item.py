a1f00a5ae9921dcc76b5612a4f02d290f163690a
Yevheniia Sholokh <yevheniiasholokh@Yevheniias-MacBook-Air.local>
2026-09-21 14:35:57 -0700

    complate first part of wave 2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,15 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            new_uuid = uuid.uuid4()
+            self.id = new_uuid.int
+        
+
+    def get_category(self):
+        category = self.__class__.__name__
+        return category
+                    
