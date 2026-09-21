51e8d8b6fe0a8b4fab6d17b80790705f5b75b566
Jie Yang <optimisty@gmail.com>
2026-09-21 15:18:35 -0700

    wave_03

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -8,8 +8,9 @@ class Item:
             new_uuid = uuid.uuid4()
             self.id = new_uuid.int
         
-
     def get_category(self):
         category = self.__class__.__name__
         return category
-                    
+
+    def __str__(self):
+        return f"An object of type Item with id {self.id}."
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -22,3 +22,17 @@ class Vendor:
                 return item
         return None    
     
+    def swap_items(self, other_vendor, my_item, their_item):
+        if (my_item not in self.inventory) or (their_item not in other_vendor.inventory):
+            return False
+        else:
+            self.remove(my_item)
+            self.add(their_item)
+            other_vendor.remove(their_item)
+            other_vendor.add(my_item)        
+            return True
+        
+        
+
+    
+    
