698b7d526bb924eab9441c09ce0ae5d08a35f114
Yevheniia Sholokh <yevheniiasholokh@Yevheniias-MacBook-Air.local>
2026-09-22 13:23:24 -0700

    madify vendor.py for wave 6

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -49,6 +49,33 @@ class Vendor:
 
         return True
 
+    def get_by_category(self, category):
+        items = []
+
+        for item in self.inventory:
+            if item.get_category() == category:
+                items.append(item)
+        return items
+
+    def get_best_by_category(self, category):
+        best_item = None
+
+        for item in self.inventory:
+            if item.get_category() == category:
+                    if best_item is None:
+                        best_item = item
+                    elif item.condition > best_item.condition:
+                        best_item = item
+        return best_item    
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+
+        if (my_best_item is None) or (their_best_item is None):
+            return False
+
+        return self.swap_items(other_vendor, my_best_item, their_best_item)
         
 
     
